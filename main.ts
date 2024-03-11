4961eea2f95b8633b81983711a9966652d52efe9
adiijo <147428794+adiijo@users.noreply.github.com>
2024-03-11 19:35:25 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.forever(function () {
+	
+})
+let line1x = 0
+let line1y = 0
+let line2x = 0
+let line2y = 0
+
+let line21x = 0
+let line21y = 0
+let line22x = 0
+let line22y = 0
+let line23x = 0
+let line23y = 0
+
+let blocktop1x = 0
+let blocktop1y = 0
+let blocktop2x = 0
+let blocktop2y = 0
+let blockbottom1x = 0
+let blockbottom1y = 0
+let blockbottom2x = 0
+let blockbottom2y = 0
